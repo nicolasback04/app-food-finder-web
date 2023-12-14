e3d3261a9b6c8324ed1b518a9b662c4ba66e7e33
Jerson Peña <jersons156@gmail.com>
2023-12-14 15:10:24 -0500

    se organiza parte de la estructura

---
+++ src/app/pages/general-process/restaurant/restaurant.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RestaurantComponent } from './restaurant/restaurant.component';
+
+
+
+@NgModule({
+  declarations: [
+    RestaurantComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class RestaurantModule { }
