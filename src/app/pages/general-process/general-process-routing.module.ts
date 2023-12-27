90258e3d8d73e39a63794ba58b0e93b179283430
Jerson-P <bolanosy8@gmail.com>
2023-12-26 22:11:33 -0500

    se realizan cambios y se agregan algunos componentes para la configuración

--- src/app/pages/general-process/general-process-routing.module.ts
+++ src/app/pages/general-process/general-process-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Routes } from '@angular/router';
 
+const routes: Routes = [
+  {
+    path: 'notifications-association',
+    data: {
+      screen: 'FACTGENASOCIANOTI',
+      title: 'Asociación de Notificaciones',
+    },
+    loadChildren: async () =>
+      (
+        await import(
+          './restaurant/restaurant.module'
+        )
+      ).RestaurantModule,
+  },
 
+]
 
 @NgModule({
   declarations: [],
